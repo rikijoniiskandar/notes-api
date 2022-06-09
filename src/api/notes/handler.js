5b57006279d3c903149ae4553a43b5d00bb2ace5
Riki Joni Iskandar <rikijoniiskandar97@gmail.com>
2022-06-10 00:21:56 +0700

    🐛 fix(notes): change response if success create note >>> ⏰ 1minute

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -29,7 +29,7 @@ class NotesHandler {
         status: "success",
         message: "Catatan berhasil ditambahkan",
         data: {
-          newNote,
+          id, title, body, tags
         },
       });
 
